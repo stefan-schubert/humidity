f679be1a853fbd2fb151a2d6b908d1f0ffb1ca77
Stefan Schubert <stfn.j.schbrt@gmail.com>
2020-04-19 21:54:29 +0200

    remove unused fonts

--- humidity_control/lib/fonts/fonts.h
+++ humidity_control/lib/fonts/fonts.h
@@ -1,8 +1,6 @@
 #include "u8g2_fonts.h"
 
 // font list
-extern const uint8_t helv_8[] U8G2_FONT_SECTION("helv_8");
-extern const uint8_t helv_14[] U8G2_FONT_SECTION("helv_14");
 extern const uint8_t dejavu_8[] U8G2_FONT_SECTION("dejavu_8");
 extern const uint8_t dejavu_14[] U8G2_FONT_SECTION("dejavu_14");
 extern const uint8_t open_iconic_symbols_4x[] U8G2_FONT_SECTION("open_iconic_symbols_4x");
